be8d9992204553f1012ad2e39152e9680120ba46
guenillon@gmail.com <guenillon@gmail.com@604baa16-6c74-744f-0772-4d0f08250b41>
2013-03-02 12:53:20 +0000

    JPIE : Version 1.1

--- trunk/Maintenance/parametres.php
+++ trunk/Maintenance/parametres.php
@@ -13,13 +13,20 @@ define("DOSSIER_EXTRACT", "./nouveau");
 define("LOG_EXTRACT", "./logs/");
 
 // Déploiement
-define("DOSSIER_SITE", "../");
-define("FILE_UPDATE_BDD", DOSSIER_EXTRACT . "/update.sql");
+define("DOSSIER_SITE", "..");
+define("DOSSIER_UPDATE_BDD", DOSSIER_EXTRACT . "/bdd");
 
 // Configuration
 define("DOSSIER_CONFIGURATION", "./configuration");
-define("DOSSIER_SITE_CONFIGURATION", DOSSIER_SITE . "configuration");
+define("DOSSIER_SITE_CONFIGURATION", DOSSIER_SITE . "/configuration");
 
-define("DOSSIER_SITE_UTILS", DOSSIER_SITE . "classes/utils/");
-define("DOSSIER_SITE_LOGS", DOSSIER_SITE . "logs/");
+define("DOSSIER_SITE_UTILS", DOSSIER_SITE . "/classes/utils/");
+define("DOSSIER_SITE_LOGS", DOSSIER_SITE . "/logs/");
+
+// La version actuelle du site
+include(DOSSIER_SITE_CONFIGURATION . "/Version.php");
+// Si la version technique n'existe pas définition à 0
+if(!defined( ZEYBUX_VERSION_TECHNIQUE ) ) {
+	define("ZEYBUX_VERSION_TECHNIQUE", 0);
+}
 ?>
