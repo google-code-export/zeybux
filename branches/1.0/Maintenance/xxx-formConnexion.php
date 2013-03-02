b345e742bfa87682990ed1bc4d2230f873c79238
guenillon@gmail.com <guenillon@gmail.com@604baa16-6c74-744f-0772-4d0f08250b41>
2013-03-02 08:45:39 +0000

    JPIE : Suppression de fichier dans la maintenance. Suite à problèmes de synchro.

--- branches/1.0/Maintenance/xxx-formConnexion.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="./connexion.php">
-     	Login : <input type="text" name="login">
-	Mot de passe : <input type="password" name="pass">
-     <input type="submit" name="connexion" value="Connexion">
-</form>
