708351c49c561c7fc686c40b8f8b7571894f82c9
guenillon@gmail.com <guenillon@gmail.com@604baa16-6c74-744f-0772-4d0f08250b41>
2013-12-29 20:49:29 +0000

    JPIE : Corrections de Bug caisse (pas de génération de l'opération d'achat) de bug de migration : les réservations en cours n'avait pas été prise en compte dans la migration et n'apparaissait pas dans la caisse. Bug de suivi des paiement : Modifiation de paiement impossible et à la validation on ne revenait pas sur le bon onglet.

--- configuration/DB.php
+++ configuration/DB.php
@@ -12,5 +12,5 @@ define("MYSQL_DB_PREFIXE", "");
 define("MYSQL_HOST", "localhost");
 define("MYSQL_LOGIN", "zeybu");
 define("MYSQL_PASS", "zeybu");
-define("MYSQL_DBNOM", "zeybu_maintenance");
+define("MYSQL_DBNOM", "zeybu_1.3");
 ?>
