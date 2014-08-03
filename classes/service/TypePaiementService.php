f68786ff5f7f463eaebe0731222f514b13d88aa5
guenillon@gmail.com <guenillon@gmail.com@604baa16-6c74-744f-0772-4d0f08250b41>
2014-08-03 18:48:01 +0000

    JPIE : Suppression de code en commentaire pour la 1.5

--- classes/service/TypePaiementService.php
+++ classes/service/TypePaiementService.php
@@ -22,64 +22,6 @@ include_once(CHEMIN_CLASSES_VALIDATEUR . "TypePaiementValid.php");
 class TypePaiementService
 {		
 	/**
-	* @name set($pTypePaiement)
-	* @param TypePaiementVO
-	* @return integer
-	* @desc Ajoute ou modifie un TypePaiement
-	*/
-	/*public function set($pTypePaiement) {
-		$lOperationValid = new OperationValid();
-		if($lOperationValid->insert($pOperation)) {			
-			return $this->insert($pOperation);			
-		} else if($lOperationValid->update($pOperation)) {
-			return $this->update($pOperation);
-		} else {
-			return false;
-		}
-	}
-	
-	/**
-	* @name insert($pOperation)
-	* @param OperationVO
-	* @return integer
-	* @desc Ajoute une opération
-	*/
-	/*private function insert($pOperation) {		
-		$lId = OperationManager::insert($pOperation); // Ajout de l'opération
-		$pOperation->setId($lId);
-		$this->insertHistorique($pOperation); // Ajout historique
-		return $lId;
-	}
-	
-	/**
-	* @name update($pOperation)
-	* @param OperationVO
-	* @return integer
-	* @desc Met à jour une opération
-	*/
-	/*private function update($pOperation) {
-		$this->insertHistorique($pOperation); // Ajout historique
-		return OperationManager::update($pOperation); // update de l'opération
-	}
-	
-	/**
-	* @name delete($pId)
-	* @param integer
-	* @desc Met à jour une opération
-	*/
-	/*public function delete($pId) {
-		$lOperationValid = new OperationValid();
-		if($lOperationValid->delete($pId)){
-			$lOperation = $this->get($pId);
-			$lOperation->setlibelle("Supression");
-			$this->insertHistorique($lOperation); // Ajout historique
-			return OperationManager::delete($pId); // delete de l'opération		
-		} else {
-			return false;
-		}
-	}
-
-	/**
 	* @name existe($pTypePaiement)
 	* @param TypePaiementVO ou interger
 	* @return bool
